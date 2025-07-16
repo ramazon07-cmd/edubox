823e72649baca65f182c217fb1b29ecc1bf24938
ramazon07-cmd <ustau2453@gmail.com>
2025-07-16 09:06:54 +0500

    task added

--- core/settings.py
+++ core/settings.py
@@ -44,8 +44,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'b92874193@gmail.com'
-EMAIL_HOST_PASSWORD = '12345678pollop'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
