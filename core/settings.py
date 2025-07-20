1c9b58774b3bf510823abc43c1cd781a05939a2b
ramazon07-cmd <ustau2453@gmail.com>
2025-07-20 09:18:04 +0500

    kk

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Load environment variables from .env file
 load_dotenv()
@@ -137,21 +138,29 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'),
-        'PORT': int(os.getenv('POSTGRES_PORT', 5432)),
-        'CONN_MAX_AGE': int(os.getenv('DB_CONN_MAX_AGE', 60)),
-        'OPTIONS': {
-            'connect_timeout': 10,
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB'),
+#         'USER': os.getenv('POSTGRES_USER'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#         'HOST': os.getenv('POSTGRES_HOST', 'db'),
+#         'PORT': int(os.getenv('POSTGRES_PORT', 5432)),
+#         'CONN_MAX_AGE': int(os.getenv('DB_CONN_MAX_AGE', 60)),
+#         'OPTIONS': {
+#             'connect_timeout': 10,
+#         }
+#     }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=int(os.getenv('DB_CONN_MAX_AGE', 60)),
+        ssl_require=True,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
